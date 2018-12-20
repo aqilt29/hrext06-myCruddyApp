09849f70fc783c462808dd75fa3d9e37211adf00
Aqil Thanawala <aqil.thanawala@gmail.com>
2018-12-19 21:33:32 -0800

    basic requirements for deletion and storage of multiple values complete

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
+  var valuesArr = [];
+
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+    valuesArr.push($('.text-entry').val())
+    localStorage.setItem('valuesArrKey', JSON.stringify(valuesArr));
+    console.log(localStorage);
+    var myItemInStorage = JSON.parse(localStorage.getItem('valuesArrKey'));
+    // console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
@@ -14,7 +18,8 @@ $(document).ready(function(){
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    localStorage.clear();
+    location.reload();
   });
 
 });
